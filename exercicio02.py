f2488e8568f55a64210608d18432ce727ff2b87c
Alexandre Montanha <alexmontanha@hotmail.com>
2026-03-26 20:42:00 -0300

    adiciona script para verificar se um ano é bissexto

---
+++ exercicio02.py
@@ -0,0 +1,8 @@
+ano = int(input("Digite o ano: "))
+
+print(f"O ano digitado é: {ano}")
+
+if ((ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0)):
+    print("O ano é bissexto.")
+else:
+    print("O ano não é bissexto.")
